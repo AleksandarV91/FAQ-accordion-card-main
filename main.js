cf3325204612877c7193460a27f9ddd53f7bed9f
AleksandarV91 <s.pilence@gmail.com>
2021-12-12 21:58:19 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", function () {
+    btn.classList.add("rotate");
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+        btn.classList.remove("rotate");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
+});
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (event) {
+//     btn.classList.toggle("rotate");
+//     let question = event.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
